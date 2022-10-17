b46edb54bb829b892e19dc72a640db45b0521a1f
Max Bigras <mbigras22@gmail.com>
2022-10-16 18:55:14 -0700

    Enable pretty print JSON

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import flask
 
 app = flask.Flask(os.environ.get("APP", __name__))
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 app.config["ENV"] = os.environ.get("ENV", "prod")
 app.config["VERSION"] = os.environ.get("VERSION", "unset")
 
