5940d75f21079a90429d9ffd0cb67216d99539c9
Max Bigras <mbigras22@gmail.com>
2022-12-29 19:24:21 -0800

    Use Hacker News Bash retry function

--- test.sh
+++ test.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-# Script test.sh tests that mbigras/app:latest Docker container can start.
+# Script test.sh tests that mbigras/app:latest Docker container can start—for explanation about Bash retry function, see https://news.ycombinator.com/item?id=34161661.
 
 set -o errexit
 
 PORT=${PORT:-8080}
-container=$(docker run --detach --env=PORT=$PORT --publish=$PORT:$PORT mbigras/app:latest)
+container=$(docker run --detach --rm --env=PORT=$PORT --publish=$PORT:$PORT mbigras/app:latest)
 
 trap "docker stop $container" EXIT
 
-while [[ $(docker inspect $container | jq -r '.[].State.Health.Status') != healthy ]]
-do
-	if [[ $(docker inspect $container | jq -r '.[].State.Health.Status') == unhealthy ]]
-	then
-		exit 1
-	fi
-	sleep 1
-done
+# For explanation about Bash retry function, see https://news.ycombinator.com/item?id=34161661.
+function retry {
+	local retries=$1
+	shift
 
-curl --fail http://localhost:$PORT/
+	local count=0
+	until "$@"
+	do
+		exit=$?
+		wait=$((2 ** $count))
+		count=$(($count + 1))
+		if [ $count -lt $retries ]
+		then
+			echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+			sleep $wait
+		else
+			echo "Retry $count/$retries exited $exit, no more retries left."
+			return $exit
+		fi
+	done
+	return 0
+}
+
+retry 10 curl --fail http://localhost:$PORT/
